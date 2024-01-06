f989356a59d4cc7ac7d4c777c68b86112ac00d87
Colin Bajak <colinbajak@gmail.com>
2024-01-06 21:02:41 +0100

    secret comment 2

--- src/main/java/net/colin/bajak/Bajak.java
+++ src/main/java/net/colin/bajak/Bajak.java
@@ -16,7 +16,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-//comment :D
+//comment :D lol
 @Mod(Bajak.MOD_ID)
 public class Bajak {
     public static final String MOD_ID = "bajak";
